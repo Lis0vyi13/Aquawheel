a6fd96f2698d395882345898cf8a94edba05f3d1
Olexandr Lisovyi <microfox141120@outlook.com>
2024-05-28 22:39:51 +0300

    increased dots accessibility

--- src/components/slider/SliderDots.jsx
+++ src/components/slider/SliderDots.jsx
@@ -1,9 +1,10 @@
 const renderDots = ({ length, slide, setSlide }) => {
   return Array.from({ length: length }, (_, index) => (
     <button
+      aria-label="slider dots navigation"
       key={index}
       onClick={() => setSlide(index)}
-      className={`w-1 h-1 rounded-full bg-white mx-1 ${
+      className={`w-2 h-2 rounded-full bg-white mx-1 ${
         index === slide ? "" : "opacity-[0.43]"
       }`}
     />
